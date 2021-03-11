ac86e5f92d826859574b4bfea98574894e7be065
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2021-03-11 14:18:18 -0500

    update director

--- scripts/dataAnalysis.py
+++ scripts/dataAnalysis.py
@@ -1,3 +1,5 @@
+"""This file contains all the functions and classes to do analysis using healpy"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
---
+++ scripts/quick_mock.py
@@ -0,0 +1,9 @@
+"""This file contains all the functions useful to generate quick mocks using healpy
+given Cls"""
+
+def
+
+
+cL_cmb_masked = np.zeros((nmocks, bins.shape[0] - 1))
+cL_elg_masked = np.zeros((nmocks, bins.shape[0] - 1))
+cL_cross_masked = np.zeros((nmocks, bins.shape[0] - 1))
